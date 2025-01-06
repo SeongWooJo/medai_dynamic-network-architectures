dd89bfc0bed6586972890214acf9b634fbf65647
SeongWoo <abc8325767@gmail.com>
2025-01-06 11:36:15 +0900

    print_log

--- dynamic_network_architectures/architectures/DANN_unet.py
+++ dynamic_network_architectures/architectures/DANN_unet.py
@@ -7,7 +7,7 @@ from dynamic_network_architectures.building_blocks.residual import BasicBlockD, 
 from dynamic_network_architectures.building_blocks.residual_encoders import ResidualEncoder
 from dynamic_network_architectures.building_blocks.unet_decoder import UNetDecoder
 from dynamic_network_architectures.building_blocks.unet_residual_decoder import UNetResDecoder
-from dynamic_network_architectures.initialization.weight_init import InitWeights_He
+from dynamic_network_architectures.initialization.dann_weight_init import dann_InitWeights_He
 from dynamic_network_architectures.initialization.weight_init import init_last_bn_before_add_to_0
 from torch import nn
 from torch.nn.modules.conv import _ConvNd
@@ -50,6 +50,7 @@ class DANNConvUNet(nn.Module):
                                                                 f"as we have resolution stages. here: {n_stages} " \
                                                                 f"stages, so it should have {n_stages - 1} entries. " \
                                                                 f"n_conv_per_stage_decoder: {n_conv_per_stage_decoder}"
+        self.nonlin = nonlin(**nonlin_kwargs)
         self.encoder = PlainConvEncoder(input_channels, n_stages, features_per_stage, conv_op, kernel_sizes, strides,
                                         n_conv_per_stage, conv_bias, norm_op, norm_op_kwargs, dropout_op,
                                         dropout_op_kwargs, nonlin, nonlin_kwargs, return_skips=True,
@@ -70,12 +71,13 @@ class DANNConvUNet(nn.Module):
             input_size = skip_sizes[-1]
         input_len = 1
         for elem in input_size:
-            print(elem)
             input_len = input_len * elem
         input_len = input_len * features_per_stage[-1]
         self.classifier = nn.Sequential(
-            nn.Linear(input_len, num_domains),
-            nn.Softmax()
+            nn.Linear(input_len, 100),
+            nn.LayerNorm(100),
+            self.nonlin,
+            nn.Linear(100, num_domains),
         )
     
     
@@ -87,119 +89,4 @@ class DANNConvUNet(nn.Module):
 
     @staticmethod
     def initialize(module):
-        InitWeights_He(1e-2)(module)
-
-
-class ResidualEncoderUNet(nn.Module):
-    def __init__(self,
-                 input_channels: int,
-                 n_stages: int,
-                 features_per_stage: Union[int, List[int], Tuple[int, ...]],
-                 conv_op: Type[_ConvNd],
-                 kernel_sizes: Union[int, List[int], Tuple[int, ...]],
-                 strides: Union[int, List[int], Tuple[int, ...]],
-                 n_blocks_per_stage: Union[int, List[int], Tuple[int, ...]],
-                 num_classes: int,
-                 n_conv_per_stage_decoder: Union[int, Tuple[int, ...], List[int]],
-                 conv_bias: bool = False,
-                 norm_op: Union[None, Type[nn.Module]] = None,
-                 norm_op_kwargs: dict = None,
-                 dropout_op: Union[None, Type[_DropoutNd]] = None,
-                 dropout_op_kwargs: dict = None,
-                 nonlin: Union[None, Type[torch.nn.Module]] = None,
-                 nonlin_kwargs: dict = None,
-                 deep_supervision: bool = False,
-                 block: Union[Type[BasicBlockD], Type[BottleneckD]] = BasicBlockD,
-                 bottleneck_channels: Union[int, List[int], Tuple[int, ...]] = None,
-                 stem_channels: int = None
-                 ):
-        super().__init__()
-        if isinstance(n_blocks_per_stage, int):
-            n_blocks_per_stage = [n_blocks_per_stage] * n_stages
-        if isinstance(n_conv_per_stage_decoder, int):
-            n_conv_per_stage_decoder = [n_conv_per_stage_decoder] * (n_stages - 1)
-        assert len(n_blocks_per_stage) == n_stages, "n_blocks_per_stage must have as many entries as we have " \
-                                                  f"resolution stages. here: {n_stages}. " \
-                                                  f"n_blocks_per_stage: {n_blocks_per_stage}"
-        assert len(n_conv_per_stage_decoder) == (n_stages - 1), "n_conv_per_stage_decoder must have one less entries " \
-                                                                f"as we have resolution stages. here: {n_stages} " \
-                                                                f"stages, so it should have {n_stages - 1} entries. " \
-                                                                f"n_conv_per_stage_decoder: {n_conv_per_stage_decoder}"
-        self.encoder = ResidualEncoder(input_channels, n_stages, features_per_stage, conv_op, kernel_sizes, strides,
-                                       n_blocks_per_stage, conv_bias, norm_op, norm_op_kwargs, dropout_op,
-                                       dropout_op_kwargs, nonlin, nonlin_kwargs, block, bottleneck_channels,
-                                       return_skips=True, disable_default_stem=False, stem_channels=stem_channels)
-        self.decoder = UNetDecoder(self.encoder, num_classes, n_conv_per_stage_decoder, deep_supervision)
-
-    def forward(self, x):
-        skips = self.encoder(x)
-        return self.decoder(skips)
-
-    def compute_conv_feature_map_size(self, input_size):
-        assert len(input_size) == convert_conv_op_to_dim(self.encoder.conv_op), "just give the image size without color/feature channels or " \
-                                                                                "batch channel. Do not give input_size=(b, c, x, y(, z)). " \
-                                                                                "Give input_size=(x, y(, z))!"
-        return self.encoder.compute_conv_feature_map_size(input_size) + self.decoder.compute_conv_feature_map_size(input_size)
-
-    @staticmethod
-    def initialize(module):
-        InitWeights_He(1e-2)(module)
-        init_last_bn_before_add_to_0(module)
-
-
-class ResidualUNet(nn.Module):
-    def __init__(self,
-                 input_channels: int,
-                 n_stages: int,
-                 features_per_stage: Union[int, List[int], Tuple[int, ...]],
-                 conv_op: Type[_ConvNd],
-                 kernel_sizes: Union[int, List[int], Tuple[int, ...]],
-                 strides: Union[int, List[int], Tuple[int, ...]],
-                 n_blocks_per_stage: Union[int, List[int], Tuple[int, ...]],
-                 num_classes: int,
-                 n_conv_per_stage_decoder: Union[int, Tuple[int, ...], List[int]],
-                 conv_bias: bool = False,
-                 norm_op: Union[None, Type[nn.Module]] = None,
-                 norm_op_kwargs: dict = None,
-                 dropout_op: Union[None, Type[_DropoutNd]] = None,
-                 dropout_op_kwargs: dict = None,
-                 nonlin: Union[None, Type[torch.nn.Module]] = None,
-                 nonlin_kwargs: dict = None,
-                 deep_supervision: bool = False,
-                 block: Union[Type[BasicBlockD], Type[BottleneckD]] = BasicBlockD,
-                 bottleneck_channels: Union[int, List[int], Tuple[int, ...]] = None,
-                 stem_channels: int = None
-                 ):
-        super().__init__()
-        if isinstance(n_blocks_per_stage, int):
-            n_blocks_per_stage = [n_blocks_per_stage] * n_stages
-        if isinstance(n_conv_per_stage_decoder, int):
-            n_conv_per_stage_decoder = [n_conv_per_stage_decoder] * (n_stages - 1)
-        assert len(n_blocks_per_stage) == n_stages, "n_blocks_per_stage must have as many entries as we have " \
-                                                  f"resolution stages. here: {n_stages}. " \
-                                                  f"n_blocks_per_stage: {n_blocks_per_stage}"
-        assert len(n_conv_per_stage_decoder) == (n_stages - 1), "n_conv_per_stage_decoder must have one less entries " \
-                                                                f"as we have resolution stages. here: {n_stages} " \
-                                                                f"stages, so it should have {n_stages - 1} entries. " \
-                                                                f"n_conv_per_stage_decoder: {n_conv_per_stage_decoder}"
-        self.encoder = ResidualEncoder(input_channels, n_stages, features_per_stage, conv_op, kernel_sizes, strides,
-                                       n_blocks_per_stage, conv_bias, norm_op, norm_op_kwargs, dropout_op,
-                                       dropout_op_kwargs, nonlin, nonlin_kwargs, block, bottleneck_channels,
-                                       return_skips=True, disable_default_stem=False, stem_channels=stem_channels)
-        self.decoder = UNetResDecoder(self.encoder, num_classes, n_conv_per_stage_decoder, deep_supervision)
-
-    def forward(self, x):
-        skips = self.encoder(x)
-        return self.decoder(skips)
-
-    def compute_conv_feature_map_size(self, input_size):
-        assert len(input_size) == convert_conv_op_to_dim(self.encoder.conv_op), "just give the image size without color/feature channels or " \
-                                                                                "batch channel. Do not give input_size=(b, c, x, y(, z)). " \
-                                                                                "Give input_size=(x, y(, z))!"
-        return self.encoder.compute_conv_feature_map_size(input_size) + self.decoder.compute_conv_feature_map_size(input_size)
-
-    @staticmethod
-    def initialize(module):
-        InitWeights_He(1e-2)(module)
-        init_last_bn_before_add_to_0(module)
-
+        dann_InitWeights_He(1e-2)(module)
