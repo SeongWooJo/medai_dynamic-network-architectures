57c39556a40511d4aa1879653500d251f83d7c8e
SeongWooJo <abc8325767@gmail.com>
2025-02-26 19:38:15 +0900

    option3

--- dynamic_network_architectures/building_blocks/embedding_layer.py
+++ dynamic_network_architectures/building_blocks/embedding_layer.py
@@ -27,11 +27,11 @@ class Embedding_Layer2(nn.Module):
         super().__init__()
 
         self.embedding_layer = nn.Sequential(
-            nn.Conv3d(feature_nums, feature_nums // 2, 1, 1, 0),  # 첫 번째 Linear Layer (입력: 512 → 출력: 256)
+            nn.Conv3d(feature_nums, feature_nums - 8, 1, 1, 0),  # 첫 번째 Linear Layer (입력: 512 → 출력: 256)
             nn.ReLU(),            # 비선형 활성화 함수
-            nn.LayerNorm(normalized_shape=(feature_nums, *input_volume_size)),    # Layer Normalization 적용
-            nn.Conv3d(feature_nums, feature_nums, 1, 1, 0),  # 두 번째 Linear Layer (출력: 128)
-            nn.LayerNorm(normalized_shape=(feature_nums, *input_volume_size)),    # 최종 Layer Normalization
+            nn.LayerNorm(normalized_shape=(feature_nums - 8, *input_volume_size)),    # Layer Normalization 적용
+            nn.Conv3d(feature_nums - 8, feature_nums - 16, 1, 1, 0),  # 두 번째 Linear Layer (출력: 128)
+            nn.LayerNorm(normalized_shape=(feature_nums - 16, *input_volume_size)),    # 최종 Layer Normalization
             nn.ReLU()
         )
         
